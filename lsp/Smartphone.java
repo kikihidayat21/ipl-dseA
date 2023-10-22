3f0e807196675153086e41908b255aea8aa338ca
Hidayat <kikihidayat21@if.unjani.ac.id>
2023-10-22 09:39:51 +0700

    perubahan sesuai prinsip liskov subs

--- lsp/Smartphone.java
+++ lsp/Smartphone.java
@@ -1,15 +1,9 @@
 package lsp;
 
-import java.sql.Date;
-
 public class Smartphone extends Product {
     @Override
     String getName(){
         return "Samsung S22 Ultra";
-    }
-    @Override
-    Date getExpiredDate(){
-        return new Date(0);
     }
 
 }
